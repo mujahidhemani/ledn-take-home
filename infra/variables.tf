23f8b8d5d98c8299972b02a271a07ab210d041ee
Mujahid Hemani <9062439+mujahidhemani@users.noreply.github.com>
2021-07-19 03:33:18 +0000

    add docker_tag var

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,11 +1,16 @@
-variable "cloudflare_zone_id" {
-  description = "Zone ID of the CloudFlare zone where record will be created"
-}
+# variable "cloudflare_zone_id" {
+#   description = "Zone ID of the CloudFlare zone where record will be created"
+# }
 
-variable "cloudflare_dns_record_name" {
-  description = "Name of the CNAME record to alias to the public load balancer record"
-}
+# variable "cloudflare_dns_record_name" {
+#   description = "Name of the CNAME record to alias to the public load balancer record"
+# }
 
-variable "tls_cert_arn" {
-  description = "The ARN of the TLS certificate to attach to the load balancer listeners"
+# variable "tls_cert_arn" {
+#   description = "The ARN of the TLS certificate to attach to the load balancer listeners"
+# }
+
+variable "docker_tag" {
+  description = "The tag of the ledn docker image to deploy"
+  default = "latest"
 }
